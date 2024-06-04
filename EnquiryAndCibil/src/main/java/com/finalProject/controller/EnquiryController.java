b3cb993c463ce8f36e4449c587d0d811cac2655b
shweta <shwetadhopre@gmail.com>
2024-06-04 16:49:19 +0530

    getAll and patch method added

--- EnquiryAndCibil/src/main/java/com/finalProject/controller/EnquiryController.java
+++ EnquiryAndCibil/src/main/java/com/finalProject/controller/EnquiryController.java
@@ -16,55 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.finalProject.model.Enquiry;
 import com.finalProject.service.Enquiry_And_CbilServiceI;
 
-
 @RestController
 public class EnquiryController {
-	
+
 	@Autowired
 	Enquiry_And_CbilServiceI es;
-	
+
 	@PostMapping("/addenquiry")
-	public ResponseEntity<Enquiry> addEnquiry(@RequestBody Enquiry e)
-	{
-		Enquiry ee=es.saveEnquiry(e);
+	public ResponseEntity<Enquiry> addEnquiry(@RequestBody Enquiry e) {
+		Enquiry ee = es.saveEnquiry(e);
 		return new ResponseEntity<Enquiry>(ee, HttpStatus.OK);
 	}
 
 	@GetMapping("/getAll")
-	public ResponseEntity<List<Enquiry>>getEnquiry()
-	{
-		List<Enquiry> enquiry =es.findAllData();
-		return new ResponseEntity<List<Enquiry>>(enquiry,HttpStatus.ACCEPTED);
-		
-	}
-	@PatchMapping("/update/{enquiryid}")
-	public ResponseEntity<Enquiry> updateData(@PathVariable String enquiryid, @RequestBody Enquiry enquiry)
-	{
-        Enquiry ed = es.updateEnquiry(enquiryid,enquiry);
-         return new ResponseEntity<Enquiry>(ed,HttpStatus.OK);
-        
-     }
-	
-	
-	
+	public ResponseEntity<List<Enquiry>> getEnquiry() {
+		List<Enquiry> enquiry = es.findAllData();
+		return new ResponseEntity<List<Enquiry>>(enquiry, HttpStatus.ACCEPTED);
 
-	
+	}
+
+	@PatchMapping("/update/{enquiryid}")
+	public ResponseEntity<Enquiry> updateData(@PathVariable String enquiryid, @RequestBody Enquiry enquiry) {
+		Enquiry ed = es.updateEnquiry(enquiryid, enquiry);
+		return new ResponseEntity<Enquiry>(ed, HttpStatus.OK);
+
+	}
 
 	@GetMapping("/getSingleEnquiry/{enquiryid}")
-	public ResponseEntity<Enquiry> getSingleEnquiryData(@PathVariable("enquiryid") String enquiryid)
-	{
-		Enquiry user=es.getSingleEnquiry(enquiryid);
-		return new ResponseEntity<Enquiry>(user,HttpStatus.OK);
+	public ResponseEntity<Enquiry> getSingleEnquiryData(@PathVariable("enquiryid") String enquiryid) {
+		Enquiry user = es.getSingleEnquiry(enquiryid);
+		return new ResponseEntity<Enquiry>(user, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/deleteEnquiry/{enquiryid}")
-	public ResponseEntity<String> deleteEnquiry(@PathVariable String enquiryid)
-	{
+	public ResponseEntity<String> deleteEnquiry(@PathVariable String enquiryid) {
 		es.deleteRecord(enquiryid);
-		
+
 		return new ResponseEntity<String>("record is deleted", HttpStatus.OK);
 	}
-	
 
 }
-
